19175f77d650190641d16b04273ec6105bc64b34
Cory Lueninghoener <cluening@gmail.com>
2021-12-23 20:53:55 -0700

    Move to a virtualenv for python

--- provision-head.sh
+++ provision-head.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-yum install -y ansible
-ansible-galaxy collection install community.kubernetes
+python3 -m venv /root/ansible-venv
+source /root/ansible-venv/bin/activate
+pip install --upgrade pip
+pip install ansible
+ansible-galaxy collection install kubernetes.core
 
-ansible-playbook -i /home/vagrant/vagrant-k3s-hpccluster/ansiblerepo/inventory -c local /home/vagrant/vagrant-k3s-hpccluster/ansiblerepo/site.yaml
+ansible-playbook \
+  -e ansible_python_interpreter=$(which python) \
+  -i /home/vagrant/vagrant-k3s-hpccluster/ansiblerepo/inventory \
+  -c local \
+  /home/vagrant/vagrant-k3s-hpccluster/ansiblerepo/site.yaml
